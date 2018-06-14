065c07d6fa4222e9a10bd9e6f9dca3d776cecce6
behrouzmoslem <behrouzmoslem@hotmail.com>
2018-06-14 11:10:51 +0330

    add_builder

--- Arch/Arch.Tests/AAA.cs
+++ Arch/Arch.Tests/AAA.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Arch.Tests
 {
-    public class AAA
+    public class AAA : IDisposable
     {
+        public AAA()
+        {
+            Arrage();
+            Act();
+        }
+        public virtual void Arrage()
+        {
+        }
+
+        public virtual void Act()
+        {
+        }
+
+        public virtual void Clean()
+        {
+        }
+
+        public void Dispose()
+        {
+            Clean();
+        }
     }
 }
---
+++ Arch/Arch.Tests/Builders/Builder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+
+namespace Arch.Tests.Builders
+{
+    public abstract class Builder<T> where T : class
+    {
+        protected T entity;
+        protected Builder()
+        {
+            entity = new Fixture().Create<T>();
+        }
+
+        public Builder<T> With<TProperty>(Expression<Func<T, TProperty>> propertyPicker, TProperty value)
+        {
+            var memberSelectorExpression = propertyPicker.Body as MemberExpression;
+            if (memberSelectorExpression != null)
+            {
+                var property = memberSelectorExpression.Member as PropertyInfo;
+                property?.SetValue(entity, value, null);
+            }
+            return this;
+        }
+
+        public virtual T Build()
+        {
+            return entity;
+        }
+
+        public virtual List<T> BuildMany(int count)
+        {
+            var res = new List<T>();
+            for (int i = 0; i < count; i++)
+                res.Add(Build());
+            return res;
+
+        }
+
+        public static implicit operator T(Builder<T> builder)
+        {
+            return builder.Build();
+        }
+    }
+}
