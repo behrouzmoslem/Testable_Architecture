e177e84b804b47b99695b2d57dae7e9cce31acce
behrouzmoslem <behrouzmoslem@hotmail.com>
2018-06-14 11:13:14 +0330

    add_fixed_classes

---
+++ Arch/Arch.IntegrationTests/AAA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arch.IntegrationTests
+{
+    public class AAA : IDisposable
+    {
+        public AAA()
+        {
+            Arrage();
+            Act();
+        }
+        public virtual void Arrage()
+        {
+        }
+
+        public virtual void Act()
+        {
+        }
+
+        public virtual void Clean()
+        {
+        }
+
+        public void Dispose()
+        {
+            Clean();
+        }
+    }
+}
